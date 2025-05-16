30d6b102f5ca5ad41ffd7e68a0d8e3f406e8fee0
MiguelDev <miguel.ang.cv95@gmail.com>
2025-05-15 23:38:40 -0500

    secciones

---
+++ src/Secciones.tsx
@@ -0,0 +1,15 @@
+export const Secciones = () => {
+  return (
+    <>
+      <header>
+        <h1>Cabecera</h1>
+      </header>
+      <section>
+        <h1>Seccion</h1>
+      </section>
+      <footer>
+        <h1>Footer</h1>
+      </footer>
+    </>
+  )
+}
